42a6534f8547c051805cdfaaf1837e8fb2b031a2
Flavie Dupré <flavie.dupre@outlook.fr>
2017-05-30 21:01:15 +0200

    first commit

---
+++ scripts/App.js
@@ -0,0 +1,3 @@
+var imgs = [ 'images/gateau1.jpg', 'images/gateau3.jpg', 'images/gateau4.jpg', 'images/gateau6.jpg', 'images/gateau7.jpg', 'images/gateau8.jpg', 'images/gateau9.jpg', 'images/gateau11.jpg', 'images/gateau13.jpg', 'images/gateau14.jpg', 'images/gateau15.jpg', 'images/gateau16.jpg', 'images/gateau17.jpg', 'images/gateau18.jpg', 'images/gateau19.jpg', 'images/gateau20.jpg'];
+
+new Memory('game', imgs);
